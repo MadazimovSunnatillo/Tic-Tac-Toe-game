103ac7261e1b620bd42ecb0febd33fee3e33f891
MadazimovSunnatillo <116077279+MadazimovSunnatillo@users.noreply.github.com>
2022-12-23 17:31:28 +0300

    finish game

---
+++ js/script.js
@@ -0,0 +1,101 @@
+let btnRef = document.querySelectorAll('.button-option');
+let popupRef = document.querySelector('.popup');
+let newGameBtn = document.querySelector('#new-game');
+let restartBtn = document.getElementsByClassName('restart')[0];
+let msgRef = document.getElementById('message');
+
+let winningPattern = [ 
+    [0,1,2],
+    [0,3,6],
+    [2,5,8],
+    [6,7,8],
+    [3,4,5],
+    [1,4,7],
+    [0,4,8],
+    [2,4,6],
+];
+
+// 'X' o'yinchisi birinchi boshlaydi
+let turnX = true;
+let count = 0;
+
+// Buttonlarni o'chirish
+const disableButtons = () => {
+    btnRef.forEach((element) => (element.disabled = true));
+    popupRef.classList.remove("hide");   
+};
+
+// Buttonlarni qayta yoqish (Yangi o'yin boshlash uchun)
+const enableButtons = () => {
+    btnRef.forEach(element => {
+        element.innerText = "";
+        element.disabled = false;
+    });
+    popupRef.classList.add("hide");
+};
+
+
+// Yangi O'yin boshlash
+newGameBtn.addEventListener("click", () => {
+    count = 0;
+    enableButtons();
+});
+
+
+restartBtn.addEventListener("click", () => {
+    count = 0;
+    enableButtons();
+});
+
+// O'yinchi yutgandan keyin ishga tushadigan funksiya
+const winFunction = (letter) => {
+    disableButtons();
+    if(letter == "X") {
+        msgRef.innerHTML = "&#x1F389; <br> 'X' Wins";
+    }else {
+        msgRef.innerHTML = "&#x1F389; <br> 'O' Wins";
+    }
+};
+
+const drawFunction = () => {
+    disableButtons();
+    msgRef.innerHTML = "&#x1EF60E; <br> It's a Draw ";
+};
+
+// Yutish logikasi
+const winChecker = () => {
+     for (let i of winningPattern) {
+        let [element1, element2, element3] = [
+            btnRef[i[0]].innerText,
+            btnRef[i[1]].innerText,
+            btnRef[i[2]].innerText,
+        ];
+
+        if (element1 != "" && (element2 != "") && (element3 != "" )) {
+            if (element1 == element2 && element2 == element3) {
+                winFunction(element1);
+            }
+        }
+     }
+};
+
+btnRef.forEach((element) => {
+    element.addEventListener("click", () => {
+        if (turnX) {
+            turnX = false;
+            element.innerText = "X";
+            element.disabled = true;
+        } else {
+            turnX = true;
+            element.innerText = "O";
+            element.disabled = true;
+        }
+        count += 1;
+        if (count == 9) {
+            drawFunction();
+        }
+        winChecker();
+    });
+});
+window.onload = enableButtons;
+
